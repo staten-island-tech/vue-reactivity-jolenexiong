936f09774d49a712eb59fd44df07394f44a93c67
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-02-09 13:14:00 -0500

    murder

--- vite-project/src/views/HomeView.vue
+++ vite-project/src/views/HomeView.vue
@@ -1,49 +1,19 @@
 <template>
   <div>
-    <DestCard v-for="destination in destinations" 
-    :key="destination.name" 
+    <SceneCard v-for="Scene in Scenes" 
+    :key="Scene.name" 
     :Destination="destination"/>
   </div>
 </template>
 
 <script setup>
-import DestCard from '@/components/icons/DestCard.vue';
-const destinations = [
+import Scene from '@/components/icons/SceneCard.vue';
+const Scene = [
   {
-    name: "Venetian Renaissance",
-    International: true,
-    price: 1300,
-    img: "https://images.unsplash.com/photo-1498307833015-e7b400441eb8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1528&q=80&quot;",
-  },
-  {
-    name: "Swiss Mountain Getaway",
-    International: true,
-    price: 800,
-    img: "https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1476&q=80&quot;",
-  },
-  {
-    name: "Ibizan Coastal Vacation",
-    International: true,
-    price: 1200,
-    img: "https://images.unsplash.com/photo-1547138494-97041dec734b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80&quot;",
-  },
-  {
-    name: "Hawaiian Vacation",
-    International: false,
-    price: 900,
-    img: "https://images.unsplash.com/photo-1507525428034-b723cf961d3e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80&quot;",
-  },
-  {
-    name: "Cascade Mountains",
-    International: false,
-    price: 400,
-    img: "https://images.unsplash.com/photo-1511497584788-876760111969?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1332&q=80&quot;",
-  },
-  {
-    name: "Egyptian Exploration",
-    International: true,
-    price: 800,
-    img: "https://images.unsplash.com/photo-1473580044384-7ba9967e16a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80&quot;",
+    answer: "Pamithi",
+    clue1: "Victum was covered in bruises.",
+    clue2: "https://manicpanic.com/cdn/shop/products/SUL15502_Raven-Human-Hair-Glam-Strip.jpg?v=1622166495&width=500",
+    clue3: "Neighbors reported hearing NightCrawler by Travis Scott."
   },
 ]
 </script>
