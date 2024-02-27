0a9446c22ee3dc09dce0cf0101c277ee64295686
jolenexiong <144476558+jolenexiong@users.noreply.github.com>
2024-02-26 23:58:26 -0500

    1

---
+++ vite-project/src/components/DestCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="about">
+   <h2>{{ Destination.name }}</h2>
+   <img :src="Destination.img" alt=""/>
+   <h3>{{ clicked }}</h3>
+   <button @click="increment">Click me</button>
+
+  </div>
+</template>
+
+<script setup>
+import {ref} from "vue";
+const props = defineProps({
+  Destination: Object,
+});
+
+//clicker logic
+const clicked = ref(0);
+function increment() {
+  clicked.valu++;
+}
+</script>
+
+<style scoped>
+img {
+  width:300px;
+  height: 350px;
+  object-fit: cover;
+}
+</style>
